7cb8e6356e451f9dcb3683c9a2164d643fe21c1a
Glenn Condron <glennc@microsoft.com>
2025-01-31 11:50:28 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     //This adds an API testing UI and makes it the default when you launch the app.
+    //This will allow you to test the endpoints easily when you run, but we also have the .http file if you prefer that.
     app.UseSwaggerUI(c=>
     {
         c.SwaggerEndpoint("/openapi/v1.json", "Weather API");
@@ -24,12 +25,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//TASK1: Use a ChatClient to generate a Joke.
 app.MapGet("/joke", () =>
 {
     return "Implement this method to use AI to generate a joke about the weather.";
 })
 .WithName("GetWeatherJoke");
 
+//TASK2: Use a ChatClient to generate a description of the weather in the given location.
 app.MapGet("/description", (string location) =>
 {
     return "Implement this method to use AI to describe the weather at the location specified, this requires tools to allow the AI to find out what the weather is in that location so it can describe it.";
